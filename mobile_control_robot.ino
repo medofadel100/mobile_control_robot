da17873e37edb3305f9a61d25a91bc4af79614f8
Ahmed Fadel <medofadel100@gmail.com>
2019-10-05 01:37:14 +0200

    Add files via upload

---
+++ mobile_control_robot.ino
@@ -0,0 +1,80 @@
+int x;
+void setup() {
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (Serial.available()> 0 )
+  {
+  x = Serial.read();
+switch(x){
+case 'F' : forward();
+break;
+case 'B' : down();
+break; 
+case 'R' : right();
+break;
+case 'L' : left();
+break; 
+case 'H' : turn;
+break;
+case 'S' : Stop();
+break;
+default: Stop();
+}}}
+void forward() 
+{
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+}
+void down() 
+{
+  digitalWrite(2,LOW );
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+}
+void left() 
+{
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+  delay(600);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+}
+void right() 
+{
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+  delay(600);
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,LOW);
+}
+void turn() 
+{
+  digitalWrite(2,HIGH);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,HIGH);
+}
+void Stop() 
+{
+  digitalWrite(2,LOW);
+  digitalWrite(3,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(5,LOW);
+}
